8650dff1310de3dfe9ed7d1471ddde0c0b59310d
Jack Thacker <jackthacker89@gmail.com>
2023-03-10 11:05:37 +0000

    Complete todo integration tests

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -1,20 +1,21 @@
 class TodoList
     def initialize
+        @task_list = []
     end
   
     def add(todo) # todo is an instance of Todo
-      # Returns nothing
+      @task_list << todo
     end
   
     def incomplete
-      # Returns all non-done todos
+      @task_list.select {|task| task.done? == false}
     end
   
     def complete
-      # Returns all complete todos
+        @task_list.select {|task| task.done? == true}
     end
   
     def give_up!
-      # Marks all todos as complete
+      @task_list.each {|task| task.mark_done!}
     end
 end
--- spec/todo_integration_spec.rb
+++ spec/todo_integration_spec.rb
@@ -1,13 +1,35 @@
-# require "todo_list"
-# require "todo"
+require "todo_list"
+require "todo"
 
-# RSpec.describe "todo_list integration" do
-#     it "Returns added tasks which are incomplete" do
-#         todo_list = TodoList.new
-#         task_1 = Todo.new("Hoover house")
-#         task_2 = Todo.new("Make bed")
-#         todo_list.add(task_1)
-#         todo_list.add(task_2)
-#         expect(todo_list.incomplete).to eq [task_1, task_2]
-#     end
-# end
+RSpec.describe "todo_list integration" do
+    it "Returns added tasks which are incomplete" do
+        todo_list = TodoList.new
+        task_1 = Todo.new("Hoover house")
+        task_2 = Todo.new("Make bed")
+        todo_list.add(task_1)
+        todo_list.add(task_2)
+        expect(todo_list.incomplete).to eq [task_1, task_2]
+    end
+
+    it "Returns added tasks which are complete" do
+        todo_list = TodoList.new
+        task_1 = Todo.new("Hoover house")
+        task_2 = Todo.new("Make bed")
+        todo_list.add(task_1)
+        todo_list.add(task_2)
+        task_2.mark_done!
+        expect(todo_list.complete).to eq [task_2]
+    end
+
+    it "Marks all tasks as complete" do
+        todo_list = TodoList.new
+        task_1 = Todo.new("Hoover house")
+        task_2 = Todo.new("Make bed")
+        todo_list.add(task_1)
+        todo_list.add(task_2)
+        todo_list.give_up!
+        expect(todo_list.incomplete).to eq []
+        expect(todo_list.complete).to eq [task_1, task_2]
+    end
+
+end
